4c69660fcd63070d3432006ba68937aec4f43c3c
vilmacio22 <vilmacio22@users.noreply.github.com>
2020-05-28 16:16:55 -0300

    tests

---
+++ test/unic/index.spec.js
@@ -0,0 +1,7 @@
+let emailSyntax = require('../../src/index');
+
+describe('Email Syntax Verify', ()=>{
+    it('should a valid email address', ()=>{
+        expect(emailSyntax.validate('example@domain.ext')).toBe(true);
+    });
+});
